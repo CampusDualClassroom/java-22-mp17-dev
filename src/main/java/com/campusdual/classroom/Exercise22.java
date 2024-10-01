552119f79e425d01eac1ca9b4e314a3c540dcf15
Julio Díaz <juliodiaz1792@gmail.com>
2024-10-01 09:56:25 +0200

    Solved exercise

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,20 +7,32 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> stringList = new ArrayList<>();
+        stringList.add("GKFFD");
+        stringList.add("TNANA");
+        stringList.add("MPMSL");
+        stringList.add("PSWME");
+        stringList.add("LZMLF");
+        stringList.add("JYEBV");
+        stringList.add("YELNT");
+        stringList.add("JSNKR");
+        stringList.add("JFESF");
+        stringList.add("TMJLL");
+        return stringList;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println(customList.get(i) + " " + i);
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        customList.add(element);
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
-
+        printElementsAndIndex(new ArrayList<>(List.of("A", "B", "C", "D")));
     }
-
-
 }
